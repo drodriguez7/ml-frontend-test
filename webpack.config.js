d390aee553d878b455c8b3c9068697c9591ca6d8
Diego Rodriguez <drodriguez96x@gmail.com>
2021-03-22 09:09:59 -0500

    Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -36,6 +37,17 @@ module.exports = {
           },
           'css-loader',
           'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[hash].[ext]',
+            },
+          },
         ],
       },
     ],
